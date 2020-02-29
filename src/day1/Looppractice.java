4528cd9411cbec79e4cad5a31e97348d51ddbfbe
Arsiema <Arsiema.ab@gmail.com>
2020-02-29 15:39:26 -0500

    Added loop practice file

---
+++ src/day1/Looppractice.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Looppractice {
+    public static void main(String[] args) {
+        //some random code
+        for (int i=0; i<10; i+=2){
+            System.out.println("Counter::" +1);
+
+        }
+    }
+}
