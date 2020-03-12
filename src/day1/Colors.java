ff82c5d99c097247a2a0e3b0652555d9dc8aecd3
Arsiema <Arsiema.ab@gmail.com>
2020-03-11 20:30:00 -0400

    Added print statement to colors class

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -4,6 +4,8 @@ public class Colors {
 
     public static void main(String[] args) {
         String[] colors = {"red", "blue", "green", "yellow"};
+        System.out.println("My colors");
+
 
     }
 }
